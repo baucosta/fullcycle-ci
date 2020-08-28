f8bcaebf56669ff98232335cd2e203ab6db01380
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2020-08-28 14:36:05 -0300

    backing hello world

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World full cycle!';
+    return 'Hello World!';
   }
 }
